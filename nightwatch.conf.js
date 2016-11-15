6b90ade3fb348648da615091e9c02e2274a934b0
leonardosouza <leonardo.souza@vivareal.com>
2016-11-15 17:29:17 -0200

    fix travis

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -5,5 +5,8 @@ module.exports = (function(settings) {
   if (process.env.SELENIUM_PORT) {
     settings.selenium.host = process.env.SELENIUM_PORT;
   }
+  if(process.env.TRAVIS_JOB_NUMBER) {
+    settings.travis.number = process.env.TRAVIS_JOB_NUMBER;
+  }
   return settings;
 })(require('./nightwatch.json'));
